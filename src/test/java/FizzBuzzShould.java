8af958daa067300a82b4af1b61c386198aaea798
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 11:19:37 +0100

    refactor: move inner class to a file

--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzShould {
@@ -8,12 +7,5 @@ public class FizzBuzzShould {
     @DisplayName("return the given number")
     public void return_the_given_number() {
         assertEquals("1", FizzBuzz.convertToFizzBuzz(1));
-    }
-
-
-    private static class FizzBuzz {
-        public static String convertToFizzBuzz(int number) {
-            return "" + number;
-        }
     }
 }
