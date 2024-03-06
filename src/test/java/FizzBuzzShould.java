30782fcebad2c1b7ac0112d8230c61ea73bdca6b
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 11:48:27 +0100

    refactor: switch to parameterized test and add more cases

--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -1,17 +1,24 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzShould {
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "1, '1'",
+            "2, '2'",
+            "4, '4'",
+            "7,'7'",
+            "8, '8'",
+            "11, '11'",
+            "13, '13'",
+            "14, '14'"
+    })
     @DisplayName("return the given number")
-    public void return_the_given_number() {
-        assertEquals("1", FizzBuzz.convertToFizzBuzz(1));
-    }
-    @Test
-    @DisplayName("return the given number 2")
-    public void return_the_given_number_2() {
-        assertEquals("2", FizzBuzz.convertToFizzBuzz(2));
+    public void return_the_given_number(int number, String convertedNumber) {
+        assertEquals(convertedNumber, FizzBuzz.convertToFizzBuzz(number));
     }
 
 }
