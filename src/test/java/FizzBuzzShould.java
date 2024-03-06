577dfd1d39bc499235908d741489d8e2800592db
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 11:11:44 +0100

    GREEN: given 1 returns '1'

--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -6,14 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzShould {
     @Test
     @DisplayName("return the given number")
-    public void return_the_given_number(){
-        assertEquals("1",FizzBuzz.convertToFizzBuzz(1));
+    public void return_the_given_number() {
+        assertEquals("1", FizzBuzz.convertToFizzBuzz(1));
     }
 
 
     private static class FizzBuzz {
         public static String convertToFizzBuzz(int number) {
-            return null;
+            return "" + number;
         }
     }
 }
