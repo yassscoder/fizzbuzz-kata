569661157fc7c4739dd1177517cc7767468594ef
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 12:52:33 +0100

    GREEN: return buzz when multiple of 5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -5,6 +5,9 @@ class FizzBuzz {
         if (number % 3 == 0) {
             return "Fizz";
         }
+        if(number%5==0){
+            return "Buzz";
+        }
         return valueOf(number);
     }
 }
