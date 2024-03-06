2e243b745a98d62af985b4e5182be71bb4afa117
yassscoder <icanmakeitonmyown@gmail.com>
2024-03-06 12:38:41 +0100

    refactor:csv source use textBlock

--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzShould {
-    @ParameterizedTest
-    @CsvSource({
-            "1, '1'",
-            "2, '2'",
-            "4, '4'",
-            "7,'7'",
-            "8, '8'",
-            "11, '11'",
-            "13, '13'",
-            "14, '14'"
-    })
+    @ParameterizedTest(name = "[{index}] {arguments}")
+    @CsvSource(useHeadersInDisplayName = true, textBlock = """
+                    number,         convertedNumber
+                    1,              1
+                    2,              2
+                    4,              4
+                    7,              7
+                    8,              8
+                    11,             11
+                    13,             13
+                    14,             14
+            """)
     @DisplayName("return the given number")
     public void return_the_given_number(int number, String convertedNumber) {
         assertEquals(convertedNumber, FizzBuzz.convertToFizzBuzz(number));
